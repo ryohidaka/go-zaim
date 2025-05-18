bcd2ecc17ef506ba6b1d01b811e61d433405d23a
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-19 00:40:37 +0900

    test(api): FetchMe() のテストコードを追加

--- go.mod
+++ go.mod
@@ -5,4 +5,11 @@ go 1.24.2
 require (
 	github.com/dghubble/oauth1 v0.7.3
 	github.com/jarcoal/httpmock v1.4.0
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ user_test.go
@@ -0,0 +1,38 @@
+package zaim_test
+
+import (
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/ryohidaka/go-zaim"
+	"github.com/ryohidaka/go-zaim/models"
+	"github.com/ryohidaka/go-zaim/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFetchMe(t *testing.T) {
+	// モックのHTTPサーバーを有効化
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// モックレスポンスを設定
+	err := testutil.MockResponseFromFile(zaim.BaseURL+"home/user/verify", "me")
+	assert.NoError(t, err)
+
+	p := zaim.NewClientParams{
+		ConsumerKey:    "dummy-key",
+		ConsumerSecret: "dummy-secret",
+		Token:          "dummy-token",
+		TokenSecret:    "dummy-secret",
+	}
+
+	// ユーザー情報を取得
+	c := zaim.NewClient(p)
+	me, err := c.FetchMe()
+
+	// レスポンスの確認
+	assert.NoError(t, err)
+	assert.Equal(t, int(me.ID), 10000000)
+	assert.Equal(t, me.Name, "MyName")
+	assert.Equal(t, me.Active, models.BoolInt(true))
+}
