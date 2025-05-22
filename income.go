c27e368df1a3fc2a8670e64559b56e1d8378549d
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-22 22:13:53 +0900

    feat(api): CreateIncome() を追加

---
+++ income.go
@@ -0,0 +1,38 @@
+package zaim
+
+import (
+	"time"
+
+	"github.com/ryohidaka/go-zaim/internal/api"
+	"github.com/ryohidaka/go-zaim/models"
+)
+
+type CreateIncomeParams struct {
+	CategoryID  uint16    `url:"category_id"`
+	Amount      int32     `url:"amount"`
+	Date        time.Time `url:"date"`
+	ReceiptID   *int64    `url:"receipt_id"`
+	ToAccountID *uint64   `url:"to_account_id,omitempty"`
+	Comment     *string   `url:"comment,omitempty"`
+	Place       *string   `url:"place,omitempty"`
+}
+
+// CreateIncome は収入情報を登録する
+func (c *Client) CreateIncome(p CreateIncomeParams) (models.Transaction, error) {
+	// レシートIDを取得
+	if p.ReceiptID == nil {
+		p.ReceiptID = api.GetReceiptID()
+	}
+
+	params, err := api.BuildQueryParams(p)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+
+	body, err := c.post("home/money/income", params)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+
+	return api.ParseTransactionResponse(body)
+}
