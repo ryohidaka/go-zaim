180ccd625047636f3d0fea7a79659f84648bc3e6
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-22 21:43:46 +0900

    feat(api): UpdatePayment() を追加

--- payment.go
+++ payment.go
@@ -1,6 +1,7 @@
 package zaim
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/ryohidaka/go-zaim/internal/api"
@@ -32,6 +33,32 @@ func (c *Client) CreatePayment(p CreatePaymentParams) (models.Transaction, error
 	}
 
 	body, err := c.post("home/money/payment", params)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+
+	return api.ParseTransactionResponse(body)
+}
+
+type UpdatePaymentParams struct {
+	Amount        int32     `url:"amount"`
+	Date          time.Time `url:"date"`
+	FromAccountID *uint64   `url:"from_account_id,omitempty"`
+	CategoryID    uint16    `url:"category_id"`
+	GenreID       uint16    `url:"genre_id"`
+	Comment       *string   `url:"comment,omitempty"`
+	Name          *string   `url:"name,omitempty"`
+}
+
+// UpdatePayment は支払情報を更新する
+func (c *Client) UpdatePayment(id uint64, p UpdatePaymentParams) (models.Transaction, error) {
+	params, err := api.BuildQueryParams(p)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+
+	path := fmt.Sprintf("home/money/payment/%d", id)
+	body, err := c.put(path, params)
 	if err != nil {
 		return models.Transaction{}, err
 	}
