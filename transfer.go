d532e5252075486b22aece0485c99ef9e65b8b7a
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-23 13:09:21 +0900

    feat(api): UpdateTransfer() を追加

--- transfer.go
+++ transfer.go
@@ -1,6 +1,7 @@
 package zaim
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/ryohidaka/go-zaim/internal/api"
@@ -23,6 +24,30 @@ func (c *Client) CreateTransfer(p CreateTransferParams) (models.Transaction, err
 	}
 
 	body, err := c.post("home/money/transfer", params)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+
+	return api.ParseTransactionResponse(body)
+}
+
+type UpdateTransferParams struct {
+	Amount        int32     `url:"amount"`
+	Date          time.Time `url:"date"`
+	FromAccountID uint64    `url:"from_account_id,omitempty"`
+	ToAccountID   uint64    `url:"to_account_id,omitempty"`
+	Comment       *string   `url:"comment,omitempty"`
+}
+
+// UpdateTransfer は振替情報を更新する
+func (c *Client) UpdateTransfer(id uint64, p UpdateTransferParams) (models.Transaction, error) {
+	params, err := api.BuildQueryParams(p)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+
+	path := fmt.Sprintf("home/money/transfer/%d", id)
+	body, err := c.put(path, params)
 	if err != nil {
 		return models.Transaction{}, err
 	}
