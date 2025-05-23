4c00a16594e8543e527ba43e77d7608186e9b329
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-23 13:11:13 +0900

    test(api): DeleteTransfer() のテストコードを追加

--- transfer_test.go
+++ transfer_test.go
@@ -188,3 +188,51 @@ func TestUpdateTransfer(t *testing.T) {
 		assert.Empty(t, res)
 	})
 }
+
+func TestDeleteTransfer(t *testing.T) {
+	// モックのHTTPサーバーを有効化
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	p := zaim.ZaimParams{
+		ConsumerKey:    "dummy-key",
+		ConsumerSecret: "dummy-secret",
+		Token:          "dummy-token",
+		TokenSecret:    "dummy-secret",
+	}
+	c := zaim.NewZaimClient(p)
+
+	t.Run("正常系: 振替情報を正しく削除できる", func(t *testing.T) {
+		// 正常なレスポンスを返すモックを設定
+		err := testutil.MockResponseFromFile("DELETE", zaim.BaseURL+"home/money/transfer/11820767", "transfer")
+		assert.NoError(t, err)
+
+		// 振替情報を削除
+		res, err := c.DeleteTransfer(11820767)
+
+		// レスポンスの確認
+		assert.NoError(t, err)
+
+		m := res.Money
+		assert.Equal(t, int(m.ID), 11820767)
+		assert.Equal(t, m.Modified, time.Date(2013, 7, 8, 21, 4, 54, 0, time.UTC))
+
+		u := res.User
+		assert.Equal(t, u.DataModified, time.Date(2013, 7, 8, 21, 4, 56, 0, time.UTC))
+		assert.Equal(t, int(u.InputCount), 12)
+
+	})
+
+	t.Run("異常系: サーバーエラー時にエラーを返却する", func(t *testing.T) {
+		// エラーを返すモックに差し替え
+		httpmock.RegisterResponder("DELETE", zaim.BaseURL+"home/money/transfer/11820767",
+			httpmock.NewStringResponder(500, `Internal Server Error`))
+
+		// 振替情報を削除
+		res, err := c.DeleteTransfer(11820767)
+
+		// レスポンスの確認
+		assert.Error(t, err)
+		assert.Empty(t, res)
+	})
+}
