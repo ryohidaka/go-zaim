9264f72e65f7f17599226749c527d96ab827ae08
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-23 13:28:43 +0900

    fix(api): CreateIncome() からレシートID登録処理を削除

--- income.go
+++ income.go
@@ -12,7 +12,6 @@ type CreateIncomeParams struct {
 	CategoryID  uint16    `url:"category_id"`
 	Amount      int32     `url:"amount"`
 	Date        time.Time `url:"date"`
-	ReceiptID   *int64    `url:"receipt_id"`
 	ToAccountID *uint64   `url:"to_account_id,omitempty"`
 	Comment     *string   `url:"comment,omitempty"`
 	Place       *string   `url:"place,omitempty"`
@@ -20,11 +19,6 @@ type CreateIncomeParams struct {
 
 // CreateIncome は収入情報を登録する
 func (c *Client) CreateIncome(p CreateIncomeParams) (models.Transaction, error) {
-	// レシートIDを取得
-	if p.ReceiptID == nil {
-		p.ReceiptID = api.GetReceiptID()
-	}
-
 	params, err := api.BuildQueryParams(p)
 	if err != nil {
 		return models.Transaction{}, err
